7dc030aa141a74c119fb3a1403276235317356bd
mmal3k <mmalekrais@gmail.com>
2024-04-27 07:51:15 +0100

    added add projects

--- src/components/User/Project/ProjectTabs/Overview.jsx
+++ src/components/User/Project/ProjectTabs/Overview.jsx
@@ -21,6 +21,39 @@ const Overview = ({ project, tasks, setTasks, isLoading, setIsLoading }) => {
           />
         )}
       </div>
+      {isLoading ? (
+        <></>
+      ) : tasks.length <=0 ? <></> :(
+        <>
+          <div className="flex justify-between p-2 w-full">
+            <img
+              className="max-w-[15rem]"
+              src="/images/overview/Group 8.svg"
+              alt="stat"
+            />
+            <img
+              className="max-w-[15rem]"
+              src="/images/overview/Group 9.svg"
+              alt="stat"
+            />
+            <img
+              className="max-w-[15rem]"
+              src="/images/overview/Group 10.svg"
+              alt="stat"
+            />
+            <img
+              className="max-w-[15rem]"
+              src="/images/overview/Group 11.svg"
+              alt="stat"
+            />
+          </div>
+          <img
+            className="self-start max-w-[30rem]"
+            src="/images/overview/Frame 1.svg"
+            alt="stats"
+          />
+        </>
+      )}
     </div>
   );
 };
